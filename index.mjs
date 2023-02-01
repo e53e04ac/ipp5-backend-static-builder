1359a9ee051705db423b5bdf3ae242ca82a57048
@e53e04ac <e53e04ac@gmail.com>
2023-02-01 22:41:22 +0900

    0

--- index.mjs
+++ index.mjs
@@ -70,7 +70,7 @@ const constructor = ((options) => {
             return _self.scriptFile().parent();
         }),
         templateDockerfile: hold(() => {
-            return _self.scriptDirectory().resolve('Dockerfile');
+            return _self.scriptDirectory().resolve('Dockerfile.template');
         }),
         runDirectory: hold(() => {
             return _options.tmpDirectory().resolve('ipp5-backend-static-builder', _options.runId());
